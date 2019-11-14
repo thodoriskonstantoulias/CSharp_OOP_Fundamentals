850500d6b146e892213a93db33460b8c85a9d839
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-11-14 20:10:15 +0200

    Created class library and properties

---
+++ ACM/ACM.BL/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public int CustomerId { get; private set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName 
+        {
+            get 
+            { 
+                return LastName + "," + FirstName;
+            }
+        }
+    }
+}
